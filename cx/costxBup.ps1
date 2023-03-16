399d0492c864936afb059746761e50d2a3f76f38
gerryRcom <gerryr@gerryr.com>
2023-03-16 22:15:52 +0000

    Add files via upload

---
+++ cx/costxBup.ps1
@@ -0,0 +1,6 @@
+﻿# Copy newest file from src to dest folder after nightly backup
+$sourcePath = "C:\DATA\Share\"
+$destPath = "C:\DATA\Target\"
+
+$sourceFile = Get-ChildItem -File $sourcePath | Sort-Object LastWriteTime -Desc | Select-Object -First 1
+Copy-Item $sourcePath$sourceFile $destPath
---
+++ cx/num2keep.ps1
@@ -0,0 +1,5 @@
+﻿# Run against a backup target folder to keep things tidy
+$targetFolder = "C:\DATA\Target"
+$num2keep = 5
+
+Get-ChildItem $targetFolder| Sort-Object LastWriteTime -Desc | Select-Object -Skip $num2keep | Remove-Item
